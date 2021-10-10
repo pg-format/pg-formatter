503896bc629ad96573419b20df359e93f5413e2e
Hirokazu Chiba <hchiba1@gmail.com>
2021-10-11 01:16:49 +0900

    Update pegjs

--- js/_formatter-v2.js
+++
@@ -1,5 +0,0 @@
-pgFormat = (input, delim = '\n  ', sep = '\n') => {
-  const parser = require('../lib/parser-v2');
-  const formatter = require('../lib/formatter.js');
-  return formatter.format(parser.parse(input), delim, sep);
-};
