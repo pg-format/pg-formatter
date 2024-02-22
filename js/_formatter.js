7b76b02ec599b93af1e5038945df4492afa245a3
Hirokazu Chiba <hchiba1@gmail.com>
2024-02-22 23:54:01 +0900

    Remove js/formatter.js

--- js/_formatter.js
+++
@@ -1,5 +0,0 @@
-pgFormat = (input, delim = '\n  ', sep = '\n') => {
-  const parser = require('../lib/parser');
-  const formatter = require('../lib/formatter.js');
-  return formatter.format(parser.parse(input), delim, sep);
-};
