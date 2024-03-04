845b6294fce5370ef5564b87e47a57ad43737c1e
Jakob Voss <voss@gbv.de>
2024-03-04 12:52:02 +0100

    Extend passing tests

--- test/parser.js
+++ test/parser.js
@@ -14,13 +14,16 @@ describe("parse examples", () => {
 });
 
 const valid = [
-  "a\r :b", // plain \r is valid line break
+  '',       // empty graph
+  'a\r :b', // plain \r is valid line break
   '""',     // empty string id
   'a :""',  // empty string label
   'a "":b', // empty string key
   'a b:""', // empty string value
-// This should be allowed to suppor plain URI as ids
-// 'http://example.org/',
+  "x\nxy\r\nxyz # comment\n\"X\"", // folded line
+  "a -> b a:\"\",2\t, -2e2,null ,\n xyz # comment", // value list
+  // This should be allowed to suppor plain URI as ids
+  // 'http://example.org/',
 ]
 
 describe("parse edge cases", () => {
@@ -42,16 +45,13 @@ const invalid = [
   'a b:',       // missing property value
   'a:',         // invalid id (must not end with colon)        
   'a b:c:d',    // not clear where key and where value
-/* These need to be detected:
-  '"',         // missing end of quoted string
-  '"\\"',     // missing end of quoted string with escaped '
-  'a :"',      // missing end of quoted string
-  'a :\'\\"',  // missing end of quoted string with escaped '
-  'a b:"',     // missing end of quoted string
-  'a b:"\\"', // missing end of quoted string with escaped '
-/* These need to be discussed:
-  '(a',       // invalid id (must not start with bracket)        
-  'a)',       // invalid id (must not end with bracket) */
+  '"',          // missing end of quoted string
+  '"\\"',       // missing end of quoted string with escaped '
+  'a :"',       // missing end of quoted string
+  'a :\'\\"',   // missing end of quoted string with escaped '
+  'a b:"',      // missing end of quoted string
+  'a b:"\\"',   // missing end of quoted string with escaped '
+  '(a',         // invalid id (must not start with bracket)        
 ]
 
 describe("detect syntax errors", () => {
