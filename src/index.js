069f8c971b2eaa1de20c05e63b6aa1b51c945adc
Hirokazu Chiba <hchiba1@gmail.com>
2024-03-21 16:00:41 +0900

    Update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 const parser = require('./parser');
 const formatter = require('./formatter.js');
 const forBlitz = require('./for-blitz.js');
+const json = require('./for-json.js');
 
-pgFormat = (input, delim = '\n  ', sep = '\n') => {
-  return formatter.format(parser.parse(input), delim, sep);
+pgFormat = (input, delim, sep) => {
+  if (delim) {
+    return formatter.format(parser.parse(input), delim, sep);
+  } else {
+    return parser.parse(input).lines.map(json.formatJSONL).join('\n');
+  }
 };
 
 pgForBlitz = (input, delim = ' ', sep = '') => {
