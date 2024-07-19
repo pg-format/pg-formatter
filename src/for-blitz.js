fadd365e1bba8a6ce46326f87c837a6b62f41b5c
Jakob Voss <voss@gbv.de>
2024-07-19 11:42:49 +0200

    Fix Blitzgraph view

--- src/for-blitz.js
+++ src/for-blitz.js
@@ -1,21 +1,21 @@
-export const formatForBlitz = graph => [
-    ...graph.nodes.map(formatNode),
-    ...graph.edges.map(formatEdge)
+export const formatForBlitz = ({nodes, edges}) => [
+    ...nodes.map(formatNode),
+    ...edges.map(formatEdge)
   ].join('\n')
 
 const formatNode = ({ id, labels, properties }) =>
-  [formatElement(id), ...labels.map(formatLabel), ...formatProperty(properties)].join(' ')
+  [formatElement(id), ...labels.map(formatLabel), ...formatProperties(properties)].join(' ')
 
 const formatEdge = ({ from, to, undirected, labels, properties }) =>
-    [`${formatElement(from)} ${undirected ? '--' : '->'} ${formatElement(to)}`, ...labels.map(formatLabel), ...formatProperty(properties)].join(' ')
+    [`${formatElement(from)} ${undirected ? '--' : '->'} ${formatElement(to)}`, ...labels.map(formatLabel), ...formatProperties(properties)].join(' ')
 
 const formatLabel = label => `:${formatElement(label)}`
 
-const formatProperty = properties => 
+const formatProperties = properties =>
   Object.entries(properties).map(([key,values]) => 
-      formatElement(key)+':'+values.map(formatValue).join(',')
-  ).join(' ')
+      formatElement(key)+':'+values.map(formatValue).join(','))
 
 // just quote everything
 const formatElement = s => JSON.stringify(s)
 const formatValue = s => typeof s === "string" ? JSON.stringify(s) : s
+
